9c1d37ebe892a58cfecc21b643c96aa981adc523
Waylon Jepsen <waylonjepsen1@gmail.com>
2021-12-22 13:23:42 -0700

    network egnostic

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,28 @@
-from brownie import accounts, FundMe, config
+from brownie import FundMe, MockV3Aggregator, config, network
 from scripts.tooling import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
+    if network.show_active() != "development":
+        print(config["networks"][network.show_active()])
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"The active netowrk is {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mocks Deployed!")
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"FundMe.sol Deployed to {fund_me.address}")
 
 
